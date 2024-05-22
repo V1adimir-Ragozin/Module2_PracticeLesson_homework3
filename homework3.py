b9ea55b871c0e781d4e84ebcd595e2b110075f84
V1adimir-Ragozin <vragozin1995@gmail.com>
2024-05-22 19:28:13 +0300

    Delete homework3.py

--- homework3.py
+++
@@ -1,7 +0,0 @@
-cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-cars_count = 0
-for i in cars:
-    print('Я езжу на автомабиле марки', i)
-for i in range(1, 21, 2):
-    cars_count += 10
-    print(cars_count)
